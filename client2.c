945175b3aa74aa2787163ceaa2479a7ad7993b70
sera0001 <107787746+sera0001@users.noreply.github.com>
2024-05-15 10:01:01 -0500

    Update client2.c

--- client2.c
+++ client2.c
@@ -10,40 +10,61 @@
 #define SERVER_IP "127.0.0.1"
 #define SERVER_PORT 2000
 
+void signup(int sockfd) ;
+
+void startGame(int sockfd) ;
+
 int main() {
     int sockfd;
     struct sockaddr_in servaddr;
 
+    // Create a socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd == -1) {
+        perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
 
+    // Initialize server address
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = inet_addr(SERVER_IP);
     servaddr.sin_port = htons(SERVER_PORT);
+
+    // Connect to the server
     if (connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) != 0) {
+        perror("Connection failed");
         exit(EXIT_FAILURE);
     }
 
+    // Client menu
     int choice;
     do {
-        char menuMessage[1024];
-        recv(sockfd, menuMessage, sizeof(menuMessage), 0);
-        printf("%s", menuMessage);
-
+        printf("\n--- Menu ---\n");
+        printf("1. Signup\n");
+        printf("2. Login\n");
+        printf("3. Play as a guest\n");
+        printf("4. Exit\n");
+        printf("Enter your choice: ");
         scanf("%d", &choice);
-        send(sockfd, &choice, sizeof(choice), 0);
+
+        // Send choice to server
+        write(sockfd, &choice, sizeof(choice));
 
         switch (choice) {
             case 1:
+                signup(sockfd);
                 break;
             case 2:
+                // Implement login functionality
                 break;
             case 3:
+                // Request game data from the server
                 char game_data[1024];
-                recv(sockfd, game_data, sizeof(game_data), 0);
+                read(sockfd, game_data, sizeof(game_data));
+
+                // Start game with received game data
+                startGame(sockfd);
                 break;
             case 4:
                 printf("Exiting program...\n");
